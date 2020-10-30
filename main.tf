034ed4b7f7820ddd070fa547e708d9fdd4609fae
jeff <jeffrey.spiers@mail.mcgill.ca>
2020-10-30 16:49:50 -0600

    local_file for kubernetes provider too

--- main.tf
+++ main.tf
@@ -20,17 +20,18 @@ data "terraform_remote_state" "cluster" {
 
 provider "kubernetes" {
   version = "~> 1.11"
-
-  load_config_file       = false
-  host                   = data.terraform_remote_state.cluster.outputs.host
-  username               = data.terraform_remote_state.cluster.outputs.username
-  password               = data.terraform_remote_state.cluster.outputs.password
-  cluster_ca_certificate = data.terraform_remote_state.cluster.outputs.cluster_ca_certificate
+  config_path = local_file.kube_config.filename
+  # load_config_file       = false
+  # host                   = data.terraform_remote_state.cluster.outputs.host
+  # username               = data.terraform_remote_state.cluster.outputs.username
+  # password               = data.terraform_remote_state.cluster.outputs.password
+  # cluster_ca_certificate = data.terraform_remote_state.cluster.outputs.cluster_ca_certificate
 }
 
 resource "local_file" "kube_config" {
-  filename = "kubeconfig.yaml"
-  content  = data.terraform_remote_state.cluster.outputs.kube_config
+  filename = "kube_config.yaml"
+  sensitive_content  = data.terraform_remote_state.cluster.outputs.kube_config
+  file_permission = "0666"
 }
 
 provider "helm" {
