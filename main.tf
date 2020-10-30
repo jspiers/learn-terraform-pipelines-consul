2d7b1a49379993f45e5cc0cc38d77044ce46bb8e
jeff <jeffrey.spiers@mail.mcgill.ca>
2020-10-30 17:21:33 -0600

    removed commented out gke stuff

--- main.tf
+++ main.tf
@@ -27,21 +27,11 @@ resource "local_file" "kube_config" {
 provider "kubernetes" {
   version = "~> 1.11"
   config_path = local_file.kube_config.filename
-  # load_config_file       = false
-  # host                   = data.terraform_remote_state.cluster.outputs.host
-  # username               = data.terraform_remote_state.cluster.outputs.username
-  # password               = data.terraform_remote_state.cluster.outputs.password
-  # cluster_ca_certificate = data.terraform_remote_state.cluster.outputs.cluster_ca_certificate
 }
 
 provider "helm" {
   version = "~> 1.0"
   kubernetes {
     config_path = local_file.kube_config.filename
-    # load_config_file       = false
-    # host                   = data.terraform_remote_state.cluster.outputs.host
-    # username               = data.terraform_remote_state.cluster.outputs.username
-    # password               = data.terraform_remote_state.cluster.outputs.password
-    # cluster_ca_certificate = data.terraform_remote_state.cluster.outputs.cluster_ca_certificate
   }
 }
