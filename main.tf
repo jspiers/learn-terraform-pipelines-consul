b2164c1474c4edae00e558a10ba49ac483a7ebb6
jeff <jeffrey.spiers@mail.mcgill.ca>
2020-10-30 20:03:53 -0600

    terraform fmt

--- main.tf
+++ main.tf
@@ -18,29 +18,10 @@ data "terraform_remote_state" "cluster" {
   }
 }
 
-# resource "local_file" "kube_config" {
-#   filename = "${path.module}/kube_config.yaml"
-#   sensitive_content = data.terraform_remote_state.cluster.outputs.kube_config
-#   # content = data.terraform_remote_state.cluster.outputs.kube_config
-#   file_permission = "0666"
-
-# }
-
-# output "kubeconfig_filename" {
-#   value = local_file.kube_config.filename
-# }
-
-# output "kubeconfig_content" {
-#   value = local_file.kube_config.content
-# }
-
 provider "kubernetes" {
-  version = "~> 1.13.3"
-  # config_path = local_file.kube_config.filename
+  version                = "~> 1.13.3"
   load_config_file       = false
   host                   = data.terraform_remote_state.cluster.outputs.host
-  # username               = data.terraform_remote_state.cluster.outputs.username
-  # password               = data.terraform_remote_state.cluster.outputs.password
   token                  = data.terraform_remote_state.cluster.outputs.token
   cluster_ca_certificate = data.terraform_remote_state.cluster.outputs.cluster_ca_certificate
 }
@@ -48,11 +29,8 @@ provider "kubernetes" {
 provider "helm" {
   version = "~> 1.0"
   kubernetes {
-    # config_path = local_file.kube_config.filename
     load_config_file       = false
     host                   = data.terraform_remote_state.cluster.outputs.host
-    # username               = data.terraform_remote_state.cluster.outputs.username
-    # password               = data.terraform_remote_state.cluster.outputs.password
     token                  = data.terraform_remote_state.cluster.outputs.token
     cluster_ca_certificate = data.terraform_remote_state.cluster.outputs.cluster_ca_certificate
   }
